ad70fdeb92b61531e612304bf42fe325b94fe44d
AhmedAbshar <116351607+AhmedAbshar@users.noreply.github.com>
2023-12-28 23:20:37 +0100

    Create My_stm32f4_uart_lib.c

---
+++ My_stm32f4_uart_lib.c
@@ -0,0 +1,20 @@
+#include "My_stm32f4_uart_lib.h"
+
+// Define your variables here
+// ...
+
+void LIB_UART_Write(char c) {
+    // Implementation for writing character to UART
+    // ...
+}
+
+char LIB_UART_Read(void) {
+    // Implementation for reading character from UART
+    // ...
+    return '\0'; // Placeholder, replace with actual implementation
+}
+
+void LIB_UART_Init(UART_ComType comType, UART_BaudrateType baudrate) {
+    // Implementation for initializing UART
+    // ...
+}
