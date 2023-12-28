13f5f7cebfc8bcb4d358f5f46ad10e3e54d839b6
AhmedAbshar <116351607+AhmedAbshar@users.noreply.github.com>
2023-12-28 23:19:08 +0100

    Create My_stm32f4_uart_driver.h

---
+++ My_stm32f4_uart_driver.h
@@ -0,0 +1,12 @@
+#ifndef MY_STM32F4_UART_DRIVER_H
+#define MY_STM32F4_UART_DRIVER_H
+
+#include <stdint.h>
+
+// Define your output pins and USART peripherals here
+
+typedef struct {
+    // Define USART registers here
+} USART_Registers;
+
+#endif // MY_STM32F4_UART_DRIVER_H
